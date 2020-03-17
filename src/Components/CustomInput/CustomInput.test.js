c9f822520e09a98b8b7a28a54897b0e7581ec363
Parth Koshta <parth.k@theghack.in>
2020-03-17 10:42:35 +0530

    - test update

--- src/Components/CustomInput/CustomInput.test.js
+++ src/Components/CustomInput/CustomInput.test.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import CustomInput from './index';
+import {shallow} from 'enzyme';
 import renderer from 'react-test-renderer';
 
 describe('CustomInput', () => {
   it('should render', () => {
     const rendered = renderer.create(<CustomInput />).toJSON();
     expect(rendered).toBeTruthy();
+  });
+
+  it('should change value', () => {
+    const onChangeText = jest.fn();
+    const wrapper = shallow(<CustomInput onChangeText={onChangeText} />);
+    wrapper.find('TextInput').simulate('changeText', 'changed text');
+    expect(onChangeText).toHaveBeenCalledWith('changed text');
   });
 });
